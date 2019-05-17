9a23ac8932cfb94731258145e682d5558e037860
Andressa Valengo <valengoandressa@gmail.com>
2019-05-17 11:08:57 -0300

    Initial Commit

---
+++ MyLearningJourney/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyLearningJourney
+//
+//  Created by Andressa Valengo on 17/05/19.
+//  Copyright © 2019 Andressa Valengo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
