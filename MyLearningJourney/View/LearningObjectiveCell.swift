c181b10c01ddd21509741f6e9185db43c5a4e35b
Andressa Valengo <valengoandressa@gmail.com>
2019-05-19 21:02:10 -0300

    added cell

---
+++ MyLearningJourney/View/LearningObjectiveCell.swift
@@ -0,0 +1,28 @@
+//
+//  LearningObjectiveCell.swift
+//  MyLearningJourney
+//
+//  Created by Andressa Valengo on 19/05/19.
+//  Copyright © 2019 Andressa Valengo. All rights reserved.
+//
+
+import UIKit
+
+class LearningObjectiveCell: UITableViewCell {
+    
+    @IBOutlet var rootView: UIView!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+
+}
